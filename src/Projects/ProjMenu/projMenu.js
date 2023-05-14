6b96e81987a4754a438028dfab20a7a36bf59cab
Vladislav205 <51053326+Vladislav205@users.noreply.github.com>
2023-05-14 21:41:28 +0300

    Add files via upload

---
+++ src/Projects/ProjMenu/projMenu.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './style.css';
+
+
+const ProjMenu = () => {
+    return ( 
+        <div className="class__home">
+            
+                <div className="main__container">
+                    <div className="main__container__menu">
+                        
+
+                    </div>
+                    <div className="main__column">
+                        <div className="task__container">
+                            ffgd;kg
+                        </div>
+                    </div>
+                </div>
+
+        </div>
+
+     );
+}
+ 
+export default ProjMenu;
---
+++ src/Projects/Projects.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './style.css';
+
+
+const Projects = () => {
+    return ( 
+        <div className="class__home">
+            
+                <div className="main__container">
+                    <div className="main__container__menu">
+                        
+
+                    </div>
+                    <div className="main__column">
+                        <div className="task__container">
+                            ffgd;kg
+                        </div>
+                    </div>
+                </div>
+
+        </div>
+
+     );
+}
+ 
+export default Projects;
