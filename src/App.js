3c002f68bf8483519f15581edbae7c00f5a0ca4a
Yakov <Oridinalscail@gmail.com>
2023-05-14 23:26:57 +0300

    jesus update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import './App.css';
+import '../src/reset.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Projects from './Projects/Projects.js';
+import Projects from './Projects/Projects';
 import NavBar from './NavBar/navBar';
 import LeftNavBar from './LeftNavBar/LeftNavBar';
-import  { useState } from 'react';
+import { useState } from 'react';
 
 
 function App() {
   return (
     <BrowserRouter>
-        
-        <div className='wrap'>
-        <NavBar/>
-        <div className='container'>
-        <LeftNavBar/>
-        <Projects/>
-        </div>
-        
-        </div>
-        {/* <Routes>
+
+      <div className='wrap'>
+        <NavBar />
+          <LeftNavBar />
+          <Projects />
+
+      </div>
+      {/* <Routes>
         <Route exact path="/" component={Home} />
         </Routes> */}
 
